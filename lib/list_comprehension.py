f5d4856894ad0d9a4d0bca804e773698b5a7fa13
Mustafa Mehmedoglu <mikbalm@gmail.com>
2024-09-03 23:36:00 -0400

    functions pass all tests!

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    '''Returns even numbers from a given list.'''
+    return [n for n in num_list if n % 2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    return [s + "!" for s in sentence_list]
+    
